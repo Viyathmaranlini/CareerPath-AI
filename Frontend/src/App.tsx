2a24eb6316d3c33f908650c1dc01eb292dbda11c
Viyathma Arukgoda <viyathmaawa@gmail.com>
2026-06-17 17:26:22 +0530

    Add Login, Signup and SavedRoadmaps routes to App

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -7,6 +7,7 @@ import AIAdvisor from './pages/AIAdvisor'
 import SkillExtinction from './pages/SkillExtinction'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import SavedRoadmaps from './pages/SavedRoadmaps'
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           <Route path="/extinction" element={<SkillExtinction />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/saved-roadmaps" element={<SavedRoadmaps />} />
         </Routes>
       </div>
     </Router>
